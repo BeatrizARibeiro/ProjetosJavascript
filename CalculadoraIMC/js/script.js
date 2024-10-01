48f7f65ddce2dd37bfc2242b1b28ac75a7fd196f
BeatrizARibeiro <biaalves.bar@gmail.com>
2024-10-01 16:23:30 -0300

    Refazendo layout das telas

--- CalculadoraIMC/js/script.js
+++ CalculadoraIMC/js/script.js
@@ -1,55 +1,66 @@
-var resp = document.getElementById('numpes')
-var resa = document.getElementById('numalt')
-var rangepeso = document.getElementById('peso')
-var rangealtura = document.getElementById('altura')
+var peso = document.getElementById('peso')
+var altura = document.getElementById('altura')
 var res = document.getElementById('res')
 
 
-resp.textContent = rangepeso.value;
-
-rangepeso.oninput = function(){
-    resp.textContent = this.value;
-}
-
-resa.textContent = rangealtura.value;
-
-rangealtura.oninput = function(){
-    resa.textContent = this.value;
-}
-
 function calcular(){
-    res.innerHTML = ""
     var imc
-    imc = rangepeso.value / ((rangealtura.value * 0.01) ** 2)
+    var check = 0;
 
-    res.innerHTML = `Seu IMC: ${imc.toFixed(2)} <br><br>`
-    if(imc < 18.5){
-        res.innerHTML += `Você está abaixo do peso <br><br>`
-        res.innerHTML += `&#128532; <br><br>`
-    }
-    else if(imc < 24.9){
-        res.innerHTML += `Você está no peso ideal <br><br>`
-        res.innerHTML += `&#128526; <br><br>`
-    }
-    else if(imc < 29.9){
-        res.innerHTML += `Você está acima do peso <br><br>`
-        res.innerHTML += `&#128532; <br><br>`
-    }
-    else if(imc < 34.9){
-        res.innerHTML += `Você está com obesidade grau I <br><br>`
-        res.innerHTML += `&#128551; <br><br>`
-    }
-    else if(imc < 39.9){
-        res.innerHTML += `Você está com obesidade grau II <br><br>`
-        res.innerHTML += `&#128551; <br><br>`
-    }
-    else {
-        res.innerHTML += `Você está com obesidade grau III <br><br>`
-        res.innerHTML += `&#128561; <br><br>`
+    var pesoConvertido = parseFloat(peso.value.replace(',', '.'))
+
+    imc = pesoConvertido / ((altura.value * 0.01) ** 2)
+
+    res.innerHTML = `<div id="linha"></div>`
+
+    if(!isNaN(parseFloat(imc)) && isFinite(imc)){
+        res.innerHTML = `Seu IMC ficou no valor de ${imc.toFixed(2).replace('.', ',')} <br><br>`
+
+        if(imc < 18.5){
+            res.innerHTML += `Você está <span class="vermelho">abaixo do peso </span><br>`
+        }
+    
+        else if(imc < 24.9){
+            res.innerHTML += `Você está <span class="verde">no peso ideal </span><br>`
+            check = 1;
+        }
+    
+        else if(imc < 29.9){
+            res.innerHTML += `Você está <span class="vermelho">acima do peso </span><br>`
+        }
+    
+        else if(imc < 34.9){
+            res.innerHTML += `Você está com <span class="vermelho">obesidade grau I </span><br>`
+        }
+    
+        else if(imc < 39.9){
+            res.innerHTML += `Você está com <span class="vermelho">obesidade grau II </span><br>`
+        }
+    
+        else {
+            res.innerHTML += `Você está com <span class="vermelho">obesidade grau III </span><br>`
+        }
+    
+        if(check > 0){
+            res.innerHTML += `<span class="material-symbols-outlined icone verde">
+                check_circle
+                </span>`
+        }
+    
+        else{
+            res.innerHTML += `<span class="material-symbols-outlined icone vermelho">
+                cancel
+            </span>`
+        }
     }
 
-
-
+    else{
+        res.innerHTML += `<span class="vermelho">Adicione um valor válido </span><br>
+        <span class="material-symbols-outlined icone vermelho">
+                cancel
+            </span>`
+    }
+   
 }
 
 
